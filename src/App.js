b88b167a109a670037a3753f1d53e45308d0f380
Thao <levunhatthao05@gmail.com>
2022-08-21 15:36:08 +0300

    data from chec

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { Container } from "@mui/system";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Products from './components/Products/Products' 
 import Navbar from "./components/Navbar/Navbar";
-
+import {commerce} from './lib/commerce';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const fetchProducts = async () => {
+    const {data} = await commerce.products.list();
+    setProducts(data);
+  }
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
   return (
     <div>
       <Container fixed>
         <Navbar />
-        <Products />
+        <Products products={products}/>
       </Container>
     </div>
   );
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import {Card, CardMedia, CardContent, CardActions, Typography} from '@mui/material';
+import {Card, CardContent, CardActions, Typography} from '@mui/material';
+import CardMedia from '@mui/material/CardMedia';
 import { AddShoppingCart } from "@mui/icons-material";
 import useStyles from './styles';
 import IconButton from '@mui/material/IconButton';
@@ -9,17 +10,17 @@ export default function Product({product}) {
  const classes = useStyles();
  return (
   <Card className={classes.root}>
-   <CardMedia className={classes.media} image={product.image} title={product.name} />
+   <CardMedia className={classes.media} image={product.media} title={product.name} />
    <CardContent>
     <div className={classes.cardContent}>
      <Typography variant="h5" gutterBottom>
        {product.name}
      </Typography>
      <Typography variant="h5">
-       {product.price}
+       {product.price.formatted_with_symbol}
      </Typography>
     </div>
-    <Typography variant="body2" color="textSecondary">{product.description}</Typography>
+    <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
    </CardContent>
    <CardActions disableSpacing className={classes.CardActions}>
     <IconButton aria-label="Add tto Cart">
